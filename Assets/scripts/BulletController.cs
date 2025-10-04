0ff8e4cbd49e26ff375686eab75d733cfce974fc
dazuki12 <alisaborzenko@gmail.com>
2025-10-05 00:14:02 +0300

    yur

--- Assets/scripts/BulletController.cs
+++ Assets/scripts/BulletController.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   public float damage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void OnTriggerEnter2D(Collider2D collision)
+   {
+      if (collision.gameObject.CompareTag("Player"))
+      {
+         collision.GetComponent<Player>().TakeDamage(damage);
+      }
+   }
 }
